20abe1f13ee7c78681cab9648384a063d7e6cd49
SB <73886345+Suraj-Bhosale@users.noreply.github.com>
2026-04-11 11:15:40 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ IG_USER_ID = "17841449038057212"
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 genai.configure(api_key=GEMINI_API_KEY)
-ai_model = genai.GenerativeModel('gemini-1.5-flash')
+ai_model = genai.GenerativeModel('gemini-2.5-flash')
 
 coins = ["BTCUSDT", "ETHUSDT"]
 
